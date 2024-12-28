b1f455fc73f2f925a1372a55f4721a84c065127d
bandhandut19 <bondon21081@gmail.com>
2024-12-28 22:07:04 +0600

    implemented logout button

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,6 +1,16 @@
 import { Link, NavLink } from "react-router";
 import logo from "../../../public/microdeft_logo.png";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
+import { removeLoggedUserToken } from "@/redux/User/userSlice";
+import { toast } from "sonner";
 const Navbar = () => {
+  const userToken = useAppSelector((state: RootState) => state.user.token);
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    dispatch(removeLoggedUserToken());
+    toast("Logged Out Successfully");
+  };
   return (
     <div className="navbar bg-purple-200">
       <div className="navbar-start">
@@ -29,7 +39,7 @@ const Navbar = () => {
               <NavLink
                 to={"/"}
                 className={({ isActive }) =>
-                  `nav-link hover:bg-[#D7DFA3] hover:scale-110 duration-300 dark:text-black rounded-none text-xl  hover:text-[#1A4862] ${
+                  `nav-link hover:bg-[#D7DFA3] hover:scale-110 duration-300 dark:text-black rounded-none text-sm  hover:text-[#1A4862] ${
                     isActive ? "nav-link-active" : ""
                   }`
                 }
@@ -38,7 +48,16 @@ const Navbar = () => {
               </NavLink>
             </li>
             <li className="text-purple-600 font-bold text-lg">
-              <NavLink to={"/all-courses"}>All Courses</NavLink>
+              <NavLink
+                to={"/all-courses"}
+                className={({ isActive }) =>
+                  `nav-link hover:bg-[#D7DFA3] hover:scale-110 duration-300 dark:text-black rounded-none text-sm  hover:text-[#1A4862] ${
+                    isActive ? "nav-link-active" : ""
+                  }`
+                }
+              >
+                All Courses
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -76,18 +95,31 @@ const Navbar = () => {
       </div>
       <div className="navbar-end text-white lg:text-lg md:text-lg text-sm font-bold flex gap-2">
         <div>
-          <Link to={"/login"}>
-            <button className="bg-purple-600 lg:px-3 lg:py-2 p-1 hover:bg-purple-400 hover:text-black ">
-              Login
+          {userToken ? (
+            <button
+              onClick={handleLogout}
+              className="bg-purple-600 lg:px-3 lg:py-2 p-1 hover:bg-purple-400 hover:text-black "
+            >
+              Logout
             </button>
-          </Link>
+          ) : (
+            <Link to={"/login"}>
+              <button className="bg-purple-600 lg:px-3 lg:py-2 p-1 hover:bg-purple-400 hover:text-black ">
+                Login
+              </button>
+            </Link>
+          )}
         </div>
         <div>
-          <Link to={"/register"}>
-            <button className="bg-purple-600 lg:px-3 lg:py-2 p-1 hover:bg-purple-400 hover:text-black">
-              Register Now!
-            </button>
-          </Link>
+          {userToken ? (
+            ""
+          ) : (
+            <Link to={"/register"}>
+              <button className="bg-purple-600 lg:px-3 lg:py-2 p-1 hover:bg-purple-400 hover:text-black">
+                Register Now!
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
