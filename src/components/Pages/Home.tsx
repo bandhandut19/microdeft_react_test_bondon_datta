76988982aa868e0cfc4d826d9e5abe08faa330a6
bandhandut19 <bondon21081@gmail.com>
2024-12-28 23:38:54 +0600

    fixed responsive issue of welcome banner for mobiles

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   );
   return (
     <div>
-      <h1 className="text-center font-bold bg-purple-600 text-white text-xl py-2">
+      <h1 className="text-center font-bold bg-purple-600 text-white lg:text-xl py-2">
         Welcome {userDetails.name} !
       </h1>
     </div>
