1b5ce3d49fec7d19c840d739b0e71bdac04cab9a
bandhandut19 <bondon21081@gmail.com>
2024-12-29 17:38:14 +0600

    fetched all courses and working on cards to display courses

--- src/components/Pages/Courses.tsx
+++ src/components/Pages/Courses.tsx
@@ -2,19 +2,50 @@
 import { useGetAllCoursesQuery } from "@/redux/Course/courseApi";
 import { useState } from "react";
 import CustomTitle from "../UI/CustomTitle";
+import ICourseInfo from "@/types/addCourseType";
 
 const Courses = () => {
   const [currentPage, setCurrentPage] = useState(0);
-  const { data, error, isLoading } = useGetAllCoursesQuery({
+  const colorMap: Record<string, string> = {
+    red: "bg-red-600",
+    blue: "bg-blue-600",
+    green: "bg-green-600",
+    yellow: "bg-yellow-600",
+    Yellow: "bg-yellow-600",
+  };
+  const { data, isLoading } = useGetAllCoursesQuery({
     params: currentPage,
   });
-  if (error) {
-    console.log(error);
-  }
-  console.log(data);
+  const courseData = data?.data?.data;
+  console.log(courseData);
   return (
-    <div>
+    <div className="px-10 min-h-screen mb-10">
       <CustomTitle title="All Courses"></CustomTitle>
+      <div className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5">
+        {courseData?.map((course: ICourseInfo) => (
+          <div key={course.id} className="card bg-purple-200 shadow-xl">
+            <img src={course.image} alt="course-image" />
+            <div className="card-body">
+              <h2 className="card-title">
+                {course.title}
+                <div
+                  className={`badge ${
+                    colorMap[course?.badge_color || "blue"]
+                  } text-white`}
+                >
+                  {course.badge_text}
+                </div>
+              </h2>
+              <p>{course.description}</p>
+            </div>
+            <div className=" card-actions justify-center bg-purple-600 text-white text-lg font-bold py-3 hover:bg-purple-300 hover:text-black border-2 cursor-pointer hover:border-purple-600 hover:transition-colors hover:duration-700">
+              <button className="text-center">
+                <h1 className="text-center">View Details</h1>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/types/addCourseType.ts
+++ src/types/addCourseType.ts
@@ -1,9 +1,11 @@
 interface ICourseInfo {
+  id?: number;
   title: string;
   description: string;
   badge_text: string;
   badge_color: string;
   instructor_name: string;
+  image?: string;
 }
 
 export default ICourseInfo;
