47ecd841aa1c8a3b091f1bd17221f50461f69e79
bandhandut19 <bondon21081@gmail.com>
2024-12-29 17:50:25 +0600

    displayed course details on cards

--- src/components/Pages/Courses.tsx
+++ src/components/Pages/Courses.tsx
@@ -3,7 +3,7 @@ import { useGetAllCoursesQuery } from "@/redux/Course/courseApi";
 import { useState } from "react";
 import CustomTitle from "../UI/CustomTitle";
 import ICourseInfo from "@/types/addCourseType";
-
+import { FaAddressBook } from "react-icons/fa";
 const Courses = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const colorMap: Record<string, string> = {
@@ -21,9 +21,12 @@ const Courses = () => {
   return (
     <div className="px-10 min-h-screen mb-10">
       <CustomTitle title="All Courses"></CustomTitle>
-      <div className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5">
+      <div className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 ">
         {courseData?.map((course: ICourseInfo) => (
-          <div key={course.id} className="card bg-purple-200 shadow-xl">
+          <div
+            key={course.id}
+            className="card bg-purple-200 shadow-xl hover:scale-105"
+          >
             <img src={course.image} alt="course-image" />
             <div className="card-body">
               <h2 className="card-title">
@@ -37,6 +40,18 @@ const Courses = () => {
                 </div>
               </h2>
               <p>{course.description}</p>
+              <div>
+                <div>
+                  <h1>
+                    <span className="text-purple-700 font-extrabold flex items-center gap-2">
+                      <FaAddressBook /> Instructor:
+                      <span className="font-bold text-purple-600">
+                        {course.instructor_name}
+                      </span>
+                    </span>{" "}
+                  </h1>
+                </div>
+              </div>
             </div>
             <div className=" card-actions justify-center bg-purple-600 text-white text-lg font-bold py-3 hover:bg-purple-300 hover:text-black border-2 cursor-pointer hover:border-purple-600 hover:transition-colors hover:duration-700">
               <button className="text-center">
