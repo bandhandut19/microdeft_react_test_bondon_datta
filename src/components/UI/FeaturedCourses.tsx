25bcb4bbe6660b65b273bd3a3895fabc5e546287
bandhandut19 <bondon21081@gmail.com>
2024-12-29 23:15:22 +0600

    protected sections for logged users

--- src/components/UI/FeaturedCourses.tsx
+++ src/components/UI/FeaturedCourses.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import CustomTitle from "./CustomTitle";
 import { useGetAllCoursesQuery } from "@/redux/Course/courseApi";
 import ICourseInfo from "@/types/addCourseType";
 import { FaAddressBook } from "react-icons/fa";
+import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
 
 const FeaturedCourses = () => {
   const colorMap: Record<string, string> = {
@@ -12,6 +13,9 @@ const FeaturedCourses = () => {
     yellow: "bg-yellow-600",
     Yellow: "bg-yellow-600",
   };
+  const userDetails = useAppSelector(
+    (state: RootState) => state.user.userDetails
+  );
   const { data } = useGetAllCoursesQuery({});
   const courseData = data?.data?.data;
   const FeaturedCourses = courseData?.filter(
@@ -23,46 +27,50 @@ const FeaturedCourses = () => {
       <h1 className="text-center text-4xl font-bold py-2 text-white bg-purple-600">
         Featured Courses
       </h1>
-      <div className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 ">
-        {FeaturedCourses?.map((course: ICourseInfo) => (
-          <div
-            key={course.id}
-            className="card bg-purple-200 shadow-xl hover:scale-105"
-          >
-            <img src={course.image} alt="course-image" />
-            <div className="card-body">
-              <h2 className="card-title">
-                {course.title}
-                <div
-                  className={`badge ${
-                    colorMap[course?.badge_color || "blue"]
-                  } text-white`}
-                >
-                  {course.badge_text}
-                </div>
-              </h2>
-              <p>{course.description}</p>
-              <div>
+      {userDetails?.email ? (
+        <div className="mt-10 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 ">
+          {FeaturedCourses?.map((course: ICourseInfo) => (
+            <div
+              key={course.id}
+              className="card bg-purple-200 shadow-xl hover:scale-105"
+            >
+              <img src={course.image} alt="course-image" />
+              <div className="card-body">
+                <h2 className="card-title">
+                  {course.title}
+                  <div
+                    className={`badge ${
+                      colorMap[course?.badge_color || "blue"]
+                    } text-white`}
+                  >
+                    {course.badge_text}
+                  </div>
+                </h2>
+                <p>{course.description}</p>
                 <div>
-                  <h1>
-                    <span className="text-purple-700 font-extrabold flex items-center gap-2">
-                      <FaAddressBook /> Instructor:
-                      <span className="font-bold text-purple-600">
-                        {course.instructor_name}
-                      </span>
-                    </span>{" "}
-                  </h1>
+                  <div>
+                    <h1>
+                      <span className="text-purple-700 font-extrabold flex items-center gap-2">
+                        <FaAddressBook /> Instructor:
+                        <span className="font-bold text-purple-600">
+                          {course.instructor_name}
+                        </span>
+                      </span>{" "}
+                    </h1>
+                  </div>
                 </div>
               </div>
+              <div className=" card-actions justify-center bg-purple-600 text-white text-lg font-bold py-3 hover:bg-purple-300 hover:text-black border-2 cursor-pointer hover:border-purple-600 hover:transition-colors hover:duration-700">
+                <button className="text-center">
+                  <h1 className="text-center">View Details</h1>
+                </button>
+              </div>
             </div>
-            <div className=" card-actions justify-center bg-purple-600 text-white text-lg font-bold py-3 hover:bg-purple-300 hover:text-black border-2 cursor-pointer hover:border-purple-600 hover:transition-colors hover:duration-700">
-              <button className="text-center">
-                <h1 className="text-center">View Details</h1>
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <h1 className="text-center text-xl font-bold mt-10">Login to view</h1>
+      )}
     </div>
   );
 };
